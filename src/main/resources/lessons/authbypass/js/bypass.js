3d2d39363d2a48231d757b5fbe50257c2f70695d
malne <malnen13@gmail.com>
2024-05-05 19:27:31 +0200

    CSRF fix

--- src/main/resources/lessons/authbypass/js/bypass.js
+++ src/main/resources/lessons/authbypass/js/bypass.js
@@ -1,15 +1,20 @@
 // need custom js for this?
 
-webgoat.customjs.onBypassResponse = function(data) {
+webgoat.customjs.onBypassResponse = function (data) {
     webgoat.customjs.jquery('#verify-account-form').hide();
     webgoat.customjs.jquery('#change-password-form').show();
 }
 
 var onViewProfile = function () {
     console.warn("on view profile activated")
+    var csrfToken = $("meta[name='_csrf']").attr("content");
+    var csrfHeader = $("meta[name='_csrf_header']").attr("content");
+    var headers = {};
+    headers[csrfHeader] = csrfToken;
     webgoat.customjs.jquery.ajax({
         method: "GET",
         url: "IDOR/profile",
+        headers,
         contentType: 'application/json; charset=UTF-8'
-     }).then(webgoat.customjs.idorViewProfile);
+    }).then(webgoat.customjs.idorViewProfile);
 }
--- src/main/resources/lessons/idor/js/idor.js
+++ src/main/resources/lessons/idor/js/idor.js
@@ -1,18 +1,23 @@
 // need custom js for this?
 
-webgoat.customjs.idorViewProfile = function(data) {
+webgoat.customjs.idorViewProfile = function (data) {
     webgoat.customjs.jquery('#idor-profile').html(
-        'name:' + data.name + '<br/>'+
-        'color:' + data.color + '<br/>'+
-        'size:' + data.size + '<br/>'
+            'name:' + data.name + '<br/>' +
+            'color:' + data.color + '<br/>' +
+            'size:' + data.size + '<br/>'
     );
 }
 
 var onViewProfile = function () {
     console.warn("on view profile activated")
+    var csrfToken = $("meta[name='_csrf']").attr("content");
+    var csrfHeader = $("meta[name='_csrf_header']").attr("content");
+    var headers = {};
+    headers[csrfHeader] = csrfToken;
     webgoat.customjs.jquery.ajax({
         method: "GET",
         url: "IDOR/profile",
+        headers,
         contentType: 'application/json; charset=UTF-8'
-     }).then(webgoat.customjs.idorViewProfile);
+    }).then(webgoat.customjs.idorViewProfile);
 }
